b3aa7a7ef6a55099460296349890c0d1901d75ef
jendralhxr <jendral.hxr@gmail.com>
2020-08-28 11:09:41 +0900

    evaluate chance of the hand

--- patlikur.c
+++ patlikur.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define STEP_MAX 4000
+#define STEP_MAX 400
 
 char card[4]; // 1 to 10
 char token[3]; //inbetween cards,
@@ -11,12 +11,29 @@ char token[3]; //inbetween cards,
 	3: division
 */
 int i;
-int val;
-
+float val;
+int step;
+	
+unsigned int evaluate_chance(){
+	unsigned int odd=1;
+	char copy[11]= {4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4};
+	
+	for (i=0; i<4; i++){
+		odd *= copy[card[i]];
+		copy[card[i]]--;	
+		}
+	return(odd);
+	}
+	
 char evaluate_set(){
 	char shuffle, temp;
 	
-	// shuffle cards
+	/*
+	 * among valid 24 combinations of the four dealt cards
+	 * swapping position of two cards will result in two combination each
+	 * so number of shuffles is decided as:
+	 * 4! / 2!
+	 */
 	for (i=0; i<12; i++){ // maximum number of shuffles
 		shuffle= rand()%7;
 		//printf("shuffle: %d\n",shuffle);
@@ -27,7 +44,7 @@ char evaluate_set(){
 			case 4: temp= card[0]; card[0]= card[2]; card[2]= temp; break;
 			case 5: temp= card[1]; card[1]= card[3]; card[3]= temp; break;
 			case 6: temp= card[0]; card[0]= card[3]; card[3]= temp; break;
-			default: break;
+			default: break; // '0' means no shuffle
 			}
 		}
 	
@@ -40,13 +57,14 @@ char evaluate_set(){
 	val = card[0];
 	for (i=0; i<3; i++){
 		switch (token[i]){
-			case 0: val+= card[i+1]; break;
-			case 1: val-= card[i+1]; break;
-			case 2: val*= card[i+1]; break;
-			case 3: val/= card[i+1]; break; // does it need to be exact factors?
+			case 0: val+= (float) card[i+1]; break;
+			case 1: val-= (float) card[i+1]; break;
+			case 2: val*= (float) card[i+1]; break;
+			case 3: val/= (float) card[i+1]; break; // does it need to be exact factors?
 			default: break;
 			}
 		}
+	
 	return(val);
 	}
 
@@ -64,9 +82,8 @@ int main(int argc, char **argv){
 			printf("%d ", card[i]);
 			}
 		}	
-	printf("deal\n");
+	printf("chance of appearance: %.4f\n", evaluate_chance()/1e4);
 			
-	int step=0; // just give up already		
 	while(step<STEP_MAX){
 		step++;
 		if (evaluate_set()==24) break;
@@ -93,5 +110,6 @@ int main(int argc, char **argv){
 	
 	}
 	
-// ended up with Monte Carlonian search
-// was considering reverse Polish notation and non-randomized exhaustive search
+// ended up with Monte Carlonian search to ensure constant number of operations in each iteration
+// was considering Reverse Polish Notation and non-randomized exhaustive search
+// todo: parenthesis rule, like (5 - (1/5)) * 5
